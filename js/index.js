6518e5d6fec185cd17326cc8bcde2965345347e0
Nguyen Ha Khanh <nguyen.hakhanh@avanquest.com>
2021-04-13 20:06:38 +0300

    Function reset all

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 /**
  * 11/04/21
- * TODO : Add button reset;
+ * TODO : Add button reset; +
  * TODO : Modal with new words;
  * TODO : Loading screen;
  */
@@ -9,6 +9,7 @@ let holderWords = document.querySelector('#js-holderWords');
 let correctAnswers = document.querySelector('#js-correctAnswers');
 let totalAnswers = document.querySelector('#js-totalAnswers');
 let btnCheck = document.querySelector('#js-btnCheck');
+let btnReset = document.querySelector('#js-btnReset');
 let inputValues; // SELECT ALL ".form-control"
 let iconForHelp;
 
@@ -98,6 +99,36 @@ btnCheck.addEventListener(
 	{ once: true }
 );
 
+// BUTTON RESET ALL
+btnReset.addEventListener('click', () => {
+	resetValuesAndStates();
+});
+
+// FUNCTION RESET ALL INPUT VALUES AND STATES
+function resetValuesAndStates() {
+	let arrForReset = inputValues;
+
+	for (let i = 0; i < arrForReset.length; i++) {
+		if (arrForReset[i].parentElement.classList.contains('is-correct')) {
+			arrForReset[i].parentElement.classList.remove('is-correct');
+		}
+		
+		if (arrForReset[i].parentElement.classList.contains('is-wrong')) {
+			arrForReset[i].parentElement.classList.remove('is-wrong');
+			arrForReset[i].nextElementSibling.remove();
+		}
+
+		if (arrForReset[i].parentElement.classList.contains('is-empty')) {
+			arrForReset[i].parentElement.classList.remove('is-empty');
+			arrForReset[i].nextElementSibling.remove();
+			arrForReset[i].previousElementSibling.remove();
+		}
+
+		arrForReset[i].value = '';
+		arrForReset[i].disabled = false;
+	}
+}
+
 // FUNCTION TAKE CLOSEST ORIGIN WORD & RETURN TRANSLATE
 function getClosestEl(el) {
 	return el.parentElement.previousElementSibling.dataset.translate;
@@ -147,7 +178,6 @@ function createTagTranslate() {
 	let tagTranslate = document.createElement('span');
 	tagTranslate.classList.add('tool-tip');
 	tagTranslate.innerHTML = this.parentElement.previousElementSibling.dataset.translate;
-	// console.log('translate', tagTranslate);
 	return tagTranslate;
 }
 
